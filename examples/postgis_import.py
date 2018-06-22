b10f3089b13ca259b30d338be5dfe992be012010
travis <travislbrundage@gmail.com>
2018-06-21 17:24:15 -0700

    idk

--- examples/postgis_import.py
+++ examples/postgis_import.py
@@ -11,20 +11,20 @@ __copyright__ = "Copyright 2012-2015 Boundless, Copyright 2010-2012 OpenPlans"
 __license__ = "MIT"
 
 from geoserver.catalog import Catalog
+import pdb
 
-cat = Catalog("http://localhost:8080/geoserver/rest", "admin", "geoserver")
-
+pdb.set_trace()
+cat = Catalog("http://192.168.99.110:8080/geoserver/rest", "admin", "geoserver")
+name = "geogig8"
 ds = cat.create_datastore(name)
+ds.type = "GeoGig"
 ds.connection_parameters.update(
-    host="localhost",
-    port="5432",
-    database="gis",
-    user="postgres",
-    passwd="",
-    dbtype="postgis")
+    geogig_repository="/vagrant/dev/.geoserver/data/geogig/geogig6",
+    branch="master",
+    create="true")
 
 cat.save(ds)
 ds = cat.get_store(name)
-components = \
-  dict((ext, "myfile." + ext) for ext in ["shp", "prj", "shx", "dbf"])
-cat.add_data_to_store(ds, "mylayer", components)
+#components = \
+#  dict((ext, "myfile." + ext) for ext in ["shp", "prj", "shx", "dbf"])
+#cat.add_data_to_store(ds, "mylayer", components)
